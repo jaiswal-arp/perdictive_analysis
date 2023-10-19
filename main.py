ded984b8ea78126ba1418e9e35cf6fbd5d3d70a9
jaiswal-arp <jaiswal.arp@northeastern.edu>
2023-10-18 22:46:52 -0400

    Adding Main Page for Streamlit App

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="ML MODELLING IN SNOWFLAKE",
+)
+
+st.write("# We have used three different ML Models here.")
+
+st.sidebar.success("Select a ML Model above.")
+
