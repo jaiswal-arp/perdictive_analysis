ce6e9cbdd6ef89f0b84755d46b9b09ad381586cf
jaiswal-arp <jaiswal.arp@northeastern.edu>
2023-10-20 17:10:32 -0400

    Updated the query fields and ran the query successfully for two fields

--- clv.py
+++ clv.py
@@ -5,14 +5,21 @@ from snowflake.snowpark.functions import *
 import json
 import numpy as np
 
+
+
 def main():
     
     # Create a session to Snowflake with credentials
     with open("creds.json") as f:
        connection_parameters = json.load(f)
     session = Session.builder.configs(connection_parameters).create()
+    session.use_warehouse("SNOWPARK_OPT_WH")
+    session.use_database("TPCDS_XGBOOST")
+    session.use_schema("DEMO")
+    st.session_state['snowpark_session'] = session
 
-
+    
+    
     # Header
     head1, head2 = st.columns([8, 1])
 
@@ -31,29 +38,35 @@ def main():
         var_value = st.sidebar.selectbox(label=var["name"], options= var["values"])
         variable_list.append(var_value)
 
-    
-        var = a = b = 0
-        match variable_list[0]:
-            case "Male":
-                 b = 1
-            case "Female": 
-                 a = 1
+    print(variable_list)
+    var = a = b = 0 
+    match variable_list[0]:
+        case "Male":
+            a = 'M'
+        case "Female": 
+            a = 'F'
                 
-        
+    match variable_list[1]:
+        case "Married":
+            b = 'M'
+        case "Single":
+            b = 'S'
+        case "Widowed": 
+            b = 'W'
+        case "Divorced": 
+            b = 'D'
+        case "Unknown": 
+            b = 'U'
            
     updated_query = data['query'].format(a= a,b = b)
     print(updated_query)    
     if st.sidebar.button('Run'):
         try:
-           result = session.sql(updated_query)
+           df_clv = session.sql(updated_query).to_pandas()
            st.write("Query executed successfully")
-           st.write(result)
+           st.write(df_clv)
         except Exception as e:
           st.error(f"Error executing the query: {str(e)}")
-       
-        
-    
-    st.write(variable_list)     
     
     
  
