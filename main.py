736e42ce213d341b43adedea78fdaf6f4594b770
jaiswal-arp <jaiswal.arp@northeastern.edu>
2023-10-21 18:26:19 -0400

    Adding main.py modifications

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import streamlit as st
-from clv import main as clv_model
-from home import main as homepage
+import roi, forecast_anomaly_detection,pcs,clv
 
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["HOME", "CLV", "PCS", "ROI"])
+page = st.sidebar.radio("Go to", ["Forecast & Anomaly Detection", "Customer Lifetime Value", "Predict Customer Spend", "Return On Investment"])
 
-if page == "CLV":
+if page == "Customer Lifetime Value":
     clv_model()
-elif page == "HOME":
+elif page == "Forecast & Anomaly Detection":
     homepage()
+    clv.main()
+    pass
+elif page == "Predict Customer Spend":
+    pcs.main()
+    pass
+elif page == "Return On Investment":
+    roi.main()
+
 
